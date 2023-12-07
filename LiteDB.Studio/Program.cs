c9d0b98c76c38dcf0ecda622da47484d59dd333c
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-12-07 20:32:33 +0300

    chore: added logic files

---
+++ LiteDB.Studio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LiteDB.Studio
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm(args.Length == 0 ? null : args[0]));
+        }
+    }
+}
